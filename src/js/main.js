63a5c8ee05bd626865c1e76ef9ca5d533827366d
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2024-04-04 16:40:23 -0300

    feat: header structure and theme switcher functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log('Website running..');
+// Switch theme
+const switchTheme = () => {
+  const rootElement = document.documentElement;
+  let dataTheme = rootElement.getAttribute('data-theme'); 
+  let newTheme;
+
+  newTheme = dataTheme === 'light' ? 'dark' : 'light';
+
+  rootElement.setAttribute('data-theme', newTheme);
+
+  localStorage.setItem('theme', newTheme);
+}
+
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme);
